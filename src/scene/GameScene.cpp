4a9eaf977026880764130d95a22a5119863fc0cd
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-30 20:21:15 +0530

    implement the snake object inside the game scene

--- src/scene/GameScene.cpp
+++ src/scene/GameScene.cpp
@@ -7,6 +7,7 @@ GameScene::~GameScene() {};
 void GameScene::draw()
 {
     food.draw(cellSize);
+    snake.draw(cellSize);
     DrawText("Game Scene", 100, 100, 20, GREEN);
     DrawText("Press Q to exit", 100, 130, 20, RED);
     DrawText("Press ENTER to play", 100, 160, 20, BLUE);
@@ -14,6 +15,11 @@ void GameScene::draw()
 
 void GameScene::update()
 {
+    snake.takeInputs();
+    if (eventTriggerForSnakeUpdate(0.2))
+    {
+        snake.update(false);
+    }
     if (IsKeyPressed(KEY_Q))
     {
         shouldExit = true;
@@ -23,6 +29,17 @@ void GameScene::update()
     {
         requestNextScene = true;
     }
+}
+
+bool GameScene::eventTriggerForSnakeUpdate(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdatedTime >= interval)
+    {
+        lastUpdatedTime = currentTime;
+        return true;
+    }
+    return false;
 }
 
 bool GameScene::shouldClose()
