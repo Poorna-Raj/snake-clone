68830ab5754996b18933452493c36e1f9d664b30
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-01 23:29:52 +0530

    add textures to the food class

--- include/object/Food.h
+++ include/object/Food.h
@@ -3,6 +3,9 @@
 
 class Food
 {
+private:
+    Texture2D texture;
+
 public:
     Vector2 position = {5, 6};
 
@@ -10,4 +13,7 @@ public:
     ~Food();
 
     void draw(int cellSize, int offsetY);
+
+    void setTexture(Texture2D text);
+    const Texture2D &getTexture() const;
 };
--- src/object/Food.cpp
+++ src/object/Food.cpp
@@ -1,10 +1,28 @@
 #include "object/Food.h"
 
-Food::Food() {};
+Food::Food()
+{
+    setTexture(LoadTexture("assets/objects/Food.png"));
+};
 
-Food::~Food() {};
+Food::~Food()
+{
+    UnloadTexture(getTexture());
+};
 
 void Food::draw(int cellSize, int offsetY)
 {
-    DrawRectangle((int)position.x * cellSize, offsetY + (int)position.y * cellSize, cellSize, cellSize, YELLOW);
+    Rectangle src = {0.0f, 0.0f, (float)getTexture().width, (float)getTexture().height};
+    Rectangle dest = {(float)position.x * cellSize, (float)offsetY + position.y * cellSize, (float)cellSize, (float)cellSize};
+    DrawTexturePro(getTexture(), src, dest, {0, 0}, 0.0f, WHITE);
+}
+
+void Food::setTexture(Texture2D text)
+{
+    texture = text;
+}
+
+const Texture2D &Food::getTexture() const
+{
+    return texture;
 }
